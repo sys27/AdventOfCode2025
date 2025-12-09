4d6997c8085e6821241db1709ca067e6a81087c4
Dmytro Kyshchenko <sys2712@gmail.com>
2025-12-09 16:07:18 +0200

    Solution for Day 9 Part 1.

---
+++ Day9/Program.cs
@@ -0,0 +1,35 @@
+﻿var file = await File.ReadAllLinesAsync("input.txt");
+var points = file
+    .Select(line =>
+    {
+        var span = line.AsSpan();
+        Span<Range> ranges = stackalloc Range[2];
+        span.Split(ranges, ',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        return new Point(
+            int.Parse(span[ranges[0]]),
+            int.Parse(span[ranges[1]])
+        );
+    })
+    .ToArray();
+
+// Part 1
+var maxArea = long.MinValue;
+for (var i = 0; i < points.Length; i++)
+{
+    for (var j = i + 1; j < points.Length; j++)
+    {
+        var p1 = points[i];
+        var p2 = points[j];
+        var area = (Math.Abs(p2.X - p1.X) + 1L) * (Math.Abs(p2.Y - p1.Y) + 1L);
+        maxArea = Math.Max(maxArea, area);
+    }
+}
+
+Console.WriteLine($"Answer for Part 1: {maxArea}");
+
+readonly record struct Point(int X, int Y)
+{
+    public override string ToString()
+        => $"({X}, {Y})";
+}
